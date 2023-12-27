af93e14d7fcf636a6728e8d95427684d0faf8ed1
saswat810 <100258729+saswat810@users.noreply.github.com>
2023-12-27 13:25:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import WishPage from './WishPage';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom'
 import Congrats from './Congrats'
-import { Container } from 'react-bootstrap'
 const App = () => {
   return (
     <Router>
