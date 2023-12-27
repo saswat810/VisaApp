604478c5426585dd2454f0e24f5233b23654d4de
saswat810 <100258729+saswat810@users.noreply.github.com>
2023-12-27 10:37:17 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import WishPage from './WishPage';
+
+const App = () => {
+  return (
+    <div>
+      <WishPage />
+    </div>
+  );
+};
+
+export default App;
